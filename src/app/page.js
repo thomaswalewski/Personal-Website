c4778750e8ec5a73661babe9b8eb763bc936e853
Thomas Walewski <93225244+thomaswalewski@users.noreply.github.com>
2023-11-12 10:23:30 -0500

    fixed svg, fix text shadow

--- src/app/page.js
+++ src/app/page.js
@@ -13,17 +13,16 @@ export default function Main() {
           <h1 className="sm:text-8xl text-5xl text-center font-semibold mt-14 cover-text">Thomas Walewski</h1>
           <div className='lg:justify-end justify-center flex'>
             <div className='w-fit mt-4'>
-              <h3 className="sm:text-2xl text-xl text-right font-semibold cover-text typewriter"> Computer Science at Northeastern.</h3>
+              <h3 className="sm:text-2xl text-l text-right font-semibold cover-text typewriter"> Computer Science at Northeastern.</h3>
             </div>
           </div>
           <div className='flex lg:justify-end justify-center'>
-            {/* <a className="sm:text-xl text-l text-right font-semibold mt-4 learn-more" href="/#about"> Learn more</a> */}
             <a className='down-arrow mt-4' href='#about'>
-              <svg fill="#ffffff" height="50px" width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="-6.6 -6.6 343.20 343.20" stroke="#ffffff" transform="rotate(90)">
+              <svg fill="#ffffff" height="50px" width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="-6.6 -6.6 343.20 343.20" stroke="#ffffff">
                 <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#323a41" strokeWidth="1.0">
                   <g id="XMLID_2_">
-                    <path id="XMLID_4_" d="M145.606,74.393c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l69.393,69.392 l-69.393,69.395c-5.858,5.858-5.858,15.355,0,21.213C127.322,258.536,131.161,260,135,260s7.678-1.464,10.606-4.394l80-80.002 c2.814-2.813,4.394-6.628,4.394-10.607c0-3.979-1.58-7.794-4.394-10.607L145.606,74.393z"></path>
-                    <path id="XMLID_5_" d="M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M165,300 c-74.439,0-135-60.561-135-135S90.561,30,165,30s135,60.561,135,135S239.439,300,165,300z"></path>
+                    <path id="XMLID_4_" d="M165,225.606c-3.839,0-7.678-1.464-10.606-4.394l-80-80c-2.93-2.929-4.394-6.767-4.394-10.606s1.464-7.678,4.394-10.606c2.928-2.929,6.767-4.394,10.606-4.394s7.678,1.465,10.606,4.394l69.393,69.392l69.393-69.392c2.929-2.929,6.767-4.394,10.606-4.394s7.678,1.465,10.606,4.394c2.93,2.928,4.394,6.767,4.394,10.606s-1.464,7.678-4.394,10.606l-80,80C172.678,224.142,168.839,225.606,165,225.606z"></path>
+                    <path id="XMLID_5_" d="M165,0c91.981,0,165,74.019,165,165s-74.019,165-165,165S0,256.981,0,165S73.019,0,165,0z M165,300c74.439,0,135-60.561,135-135S239.439,30,165,30S30,90.561,30,165S90.561,300,165,300z"></path>
                   </g>
                 </g>
               </svg>
